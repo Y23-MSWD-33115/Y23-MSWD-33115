7f3655d64ae34f5fcbb0888758313d2b1dd054f9
Y23-MSWD-33115 <roshanananthasai@gmail.com>
2024-08-06 22:58:45 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Student from './Components.js/Student';
+import Header from './Components.js/Header';
+import Hello from './Components.js/Hello';
+import Courses from './Components.js/Courses';
+import Footer from './Components.js/Footer';
+
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <Header/>
+
+      <Footer/>
+
+      <Student/>
+
+      <Courses/>
+
+      <Hello/>
+    </div>
+  );
+}
+
+export default App;
